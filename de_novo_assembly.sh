af345183f42447bdaaf82d01b0a3da587e1784be
Austin Manny <austin.manny@gmail.com>
2019-05-29 17:20:57 -0400

    Created a de novo assembly wrapper script

---
+++ de_novo_assembly.sh
@@ -0,0 +1,201 @@
+#!/bin/bash
+
+function usage() {
+    #==== FUNCTION ================================================================================#
+    #        NAME: usage
+    # DESCRIPTION: setup a usage statement that will inform the user how to correctly invoke the
+    #              program
+    #==============================================================================================#
+
+    echo -e "\nERROR: Missing samples and/or program type. \n" \
+            "Make sure to provide path samples \n\n" \
+            "Usage for paired-end reads: \n" \
+            "$0 -p dna|rna -1 path/to/reads_R1.fq -2 path/to/reads_R2.fq \n\n" \
+            "Usage in unpaired mode: \n" \
+            "$0 -p dna|rna -u path/to/unpaired_reads.fq \n\n" \
+            "Optional parameters: \n" \
+                  "-o (directory for saving the files at the end; [default=current folder])" \
+                  "-m (maximum amount of memory to use [in GB]; [default=16] ) \n" \
+                  "-n (number of CPUs/processors/cores to use; [default=use all available]) \n" \
+                  "-t (temporary directory for storing temp files; [default='/tmp/'])"
+            "Example of a complex run: \n" \
+            "$0 -SRR1001,SRR10002 -d ~/Desktop/sra_files/ -m 30 -n 6 -t /tmp/ \n\n" \
+            "Exiting program. Please retry with corrected parameters..." >&2; exit 1;
+}
+
+#==================================================================================================#
+# Make sure the pipeline is invoked correctly, with project and sample names
+#==================================================================================================#
+    while getopts "p:1:2:u:o:m:n:t:" arg;
+        do
+        	case ${arg} in
+
+                p ) # Program Type (dna or rna)
+                    MOLECULE_TYPE=(${OPTARG})
+
+                    if [[ ${MOLECULE_TYPE} == "dna" ]]; then
+                        SPADES_PROGRAM="spades.py"
+                    elif [[ ${MOLECULE_TYPE} == "rna" ]]; then
+                        SPADES_PROGRAM="rnaspades.py"
+                    else
+                           echo "ERROR: SPAdes program type must be 'dna' or 'rna' (no quotes)";
+                           echo "Exiting..." >&2;
+                           exit 3;
+                    fi
+                        ;;
+
+                1 ) # Samples: if paired, give the path and full filename for the forward read
+                    FORWARD_READS=(${OPTARG})
+                        ;;
+
+                2 ) # Samples: if paired, give the path and full filename for the reverse read
+                    REVERSE_READS=(${OPTARG})
+                        ;;
+
+                u ) # Samples: if unpaired, give the path and full filename for the reads
+                    UNPAIRED_READS=(${OPTARG})
+                        ;;
+
+                o ) # set output directory, for where to save files to
+                    OUTPUT_DIRECTORY=${OPTARG}
+                        ;;
+
+                m ) # set max memory to use (in GB; if any letters are entered, discard those)
+                    MEMORY_ENTERED=${OPTARG}
+                    MEMORY_TO_USE=$(echo $MEMORY_ENTERED | sed 's/[^0-9]*//g')
+                        ;;
+
+                n ) # set number of CPUs to use
+                    NUM_THREADS=${OPTARG}
+                        ;;
+
+                t ) # set temporary directory
+                    TEMP_DIR=${OPTARG}
+                        ;;
+
+                * ) # Display help
+        		    usage
+        		     	;;
+        	esac
+        done; shift $(( OPTIND-1 ))
+
+#==================================================================================================#
+# Check that necessary software is installed
+#==================================================================================================#
+{ command -v rnaspades.py > /dev/null || command -v spades.py > /dev/null } || \
+{   echo -e "ERROR: This requires 'spades' or 'rnaspades' de novo assemblers, but it could not found. \n" \
+        "Please install these applications. \n" \
+        "Exiting..." >&2; exit 6
+    }
+
+#==================================================================================================#
+# Process parameters: use the user-provided parameters above to create variable names that can be
+#                     called in the rest of the pipeline
+#==================================================================================================#
+
+    #==============================================================================================#
+    #  Make sure required arguments are provided
+    #==============================================================================================#
+
+    # If no SPAdes program type is provided, tell that to the user & exit
+    if [[ -z "${SPADES_PROGRAM}" ]] ; then
+     usage
+    fi
+
+    if { [[ -z ${FORWARD_READS} ]] && [[ -z ${REVERSE_READS} ]] } && \
+         [[ -z ${UNPAIRED_READS} ]]; then
+             usage
+             echo "No complete library of reads provided. Exiting..."
+             exit 9
+
+    #==============================================================================================#
+    # Output directory
+    #==============================================================================================#
+
+    # If user didn't provide out dir, then check to see if there's an
+    # SRA_DIR environmental variable, which I set on the lab iMac; if not, just use current directory
+    if [[ -z "${OUTPUT_DIRECTORY}" ]]; then
+        OUTPUT_DIRECTORY="./"
+    fi
+
+    #==============================================================================================#
+    # Temporary directory
+    #==============================================================================================#
+    if [[ -z "${TEMP_DIR}" ]]; then
+        TEMP_DIR="/tmp/"
+    fi
+
+    #==============================================================================================#
+    # Set up number of CPUs to use and RAM
+    #==============================================================================================#
+    # CPUs (aka threads aka processors aka cores):
+    ## If provided by user, use that. Otherwise:
+    ## Use `nproc` if installed (Linux or MacOS with gnu-core-utils); otherwise use `sysctl`
+    if [[ -z "${NUM_THREADS}" ]] ; then
+        {   command -v nproc > /dev/null && \
+            NUM_THREADS=`nproc` && \
+            echo "Number of processors available (according to nproc): ${NUM_THREADS}"; \
+            } \
+        || \
+        {   command -v sysctl > /dev/null && \
+            NUM_THREADS=`sysctl -n hw.ncpu` && \
+            echo "Number of processors available (according to sysctl): ${NUM_THREADS}";
+            }
+    fi
+    #==============================================================================================#
+    # Set memory usage to 16GB if none given by user
+    if [[ -z ${MEMORY_TO_USE} ]]; then
+        echo "No memory limit set by user. Defaulting to 16GB"
+        MEMORY_TO_USE="16"
+    fi
+
+    # As a check to the user, print the project name and sample numbers to the screen
+    echo "SRA sample accessions: ${ALL_SAMPLES[@]}"
+    echo "Memory to use: ${MEMORY_TO_USE}"
+    echo "Number of processors to use: ${NUM_THREADS}"
+#==================================================================================================#
+
+function de_novo_assembly() {
+    #==============================================================================================#
+    # Construct contigs from the raw reads using SPAdes
+    #==============================================================================================#
+
+    # Paired reads
+    if { [[ ! -z ${FORWARD_READS} ]] && \
+         [[ ! -z ${REVERSE_READS }]] }; then
+
+        ${SPADES_PROGRAM} \
+        -o ${OUTPUT_DIRECTORY} \
+        -1 ${FORWARD_READS \
+        -2 ${REVERSE_READS} \
+        --threads ${NUM_THREADS} \
+        -m ${MEMORY_TO_USE} \
+        --tmp-dir ${TEMP_DIR};
+
+    # Unpaired reads
+    elif [[ ! -z ${UNPAIRED_READS} ]]; then
+        ${SPADES_PROGRAM} \
+        -o ${OUTPUT_DIRECTORY} \
+        -s ${UNPAIRED_READS} \
+        --threads ${NUM_THREADS} \
+        -m ${MEMORY_TO_USE} \
+        --tmp-dir ${TEMP_DIR};
+
+    else
+        echo "Encountered unknown problem when trying to run de novo assembly."
+        echo "Could not determine library type (paired or unpaired). Exiting..."
+        usage
+        exit 9
+    #==============================================================================================#
+}
+
+#==================================================================================================#
+# Run the de novo aseembly
+#==================================================================================================#
+de_novo_assembly
+#==================================================================================================#
+
+#==================================================================================================#
+# Program complete
+#==================================================================================================#
+echo "Successfully assembled contigs. Files located at: ${OUTPUT_DIRECTORY}"
