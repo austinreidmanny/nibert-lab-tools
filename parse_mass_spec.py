b8fb3980191b7314f2dc4dd79dd4564f05ab2ed7
Austin Manny <austin.manny@gmail.com>
2022-01-20 17:48:15 -0500

    Moved the input arguments parsing code into its own function; made a dedicated main() function; overall improved the code resilience

--- parse_mass_spec.py
+++ parse_mass_spec.py
@@ -24,35 +24,39 @@ import argparse
 import io
 import os
 import pandas
+from matplotlib import pyplot
 
 import Bio.SeqRecord
 from Bio import Seq
 from Bio import AlignIO
 from Bio import SeqIO
+from Bio import Align
 from Bio.Align.Applications import MafftCommandline
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Read inputs
 # ---------------------------------------------------------------------------------------------------------------------
-# Initialize argparse
-parser = argparse.ArgumentParser()
+def read_inputs():
 
-# Read in the peptides files
-parser.add_argument('--peptides', type=argparse.FileType('r'), required=True)
+    # Initialize argparse
+    parser = argparse.ArgumentParser()
 
-# Read in the protein reference file
-parser.add_argument('--reference', type=argparse.FileType('r'), required=True)
+    # Read in the peptides files
+    parser.add_argument('--peptides', type=argparse.FileType('r'), required=True)
 
-# Save the provided inputs
-args = parser.parse_args()
-peptides_file = args.peptides
-reference_sequence = args.reference
+    # Read in the protein reference file
+    parser.add_argument('--reference', type=argparse.FileType('r'), required=True)
+
+    # Save the provided inputs
+    args = parser.parse_args()
+    return args
 
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Extract peptide strings from peptide file
 # ---------------------------------------------------------------------------------------------------------------------
-def extract_peptide_strings():
+def extract_peptide_strings(peptides_file):
 
     # Create an empty list to store all of the peptides
     cleaned_peptides = []
@@ -79,14 +83,16 @@ def extract_peptide_strings():
     # Save cleaned peptides to a fasta file
     SeqIO.write(cleaned_peptides, 'cleaned_peptides.fasta', 'fasta')
 
+    return 'cleaned_peptides.fasta'
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Take in the cleaned peptides fasta file and align peptides to each other
 # ---------------------------------------------------------------------------------------------------------------------
-def align_sequences(peptides_fasta):
+def align_sequences(reference_filename, peptides_fasta):
 
     # Read in reference sequence and cleaned peptides and combine into one master fasta
-    ref_protein = SeqIO.parse(reference_sequence, 'fasta')
+    ref_protein = SeqIO.parse(reference_filename, 'fasta')
     peptides = SeqIO.parse(peptides_fasta, 'fasta')
     SeqIO.write(ref_protein, open('combined_proteins.fasta', 'w'), 'fasta')
     SeqIO.write(peptides, open('combined_proteins.fasta', 'a'), 'fasta')
@@ -95,15 +101,78 @@ def align_sequences(peptides_fasta):
     mafft_cline = MafftCommandline(input='combined_proteins.fasta')
     stdout, stderr = mafft_cline()
     align = AlignIO.read(io.StringIO(stdout), 'fasta')
-    print(align)
 
+    # Write alignment to a fasta file
     AlignIO.write(align, 'aligned_peptides.fasta', 'fasta')
+
+    # Delete the concatenated fasta file, no use for it post-analysis
     os.remove('combined_proteins.fasta')
+
+    # Return the alignment for visualization
+    return align
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Visualization
+# ---------------------------------------------------------------------------------------------------------------------
+def visualize_alignment(multiple_sequence_alignment):
+
+    msa = multiple_sequence_alignment
+
+    # Iterate through the alignment, sequence by sequence. If gap (-), that position is a 0;
+    # If aa residue, that position is a >1;
+    # Now, I want non-overlapping lines, such that every sequence gets its own line;
+    # So it's an aa residue, that position will get assigned the sequence_counter value,
+    #   which increases by 1 every time we look at the next sequence
+
+    sequence_counter = 0
+    my_df = pandas.DataFrame(columns=['Sequence_ID', 'Position', 'Value'])
+
+    for record in msa:
+
+        sequence_counter += 1
+        residue_position = 0
+
+        seq_id = record.id
+        sequence = record.seq
+
+        for residue in sequence:
+            residue_position += 1
+
+            if residue == "-":
+                residue_value = 0
+            else:
+                residue_value = sequence_counter
+
+            my_df = my_df.append(
+                {'Sequence_ID': seq_id,
+                 'Position': residue_position,
+                 'Value': residue_value},
+                ignore_index=True
+            )
+
+    my_df.plot(kind='scatter', x='Position', y='Value')
+    pyplot.show()
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Main code
 # ---------------------------------------------------------------------------------------------------------------------
-# Run the main function
+def main():
+
+    # Take in the user-provided inputs
+    args = read_inputs()
+    peptides_file = args.peptides
+    reference_file = args.reference
+
+    # Run the analysis
+    cleaned_peptides = extract_peptide_strings(peptides_file)
+    multiple_sequence_alignment = align_sequences(reference_file, cleaned_peptides)
+    visualize_alignment(multiple_sequence_alignment)
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Execute code
+# ---------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    extract_peptide_strings()
-    align_sequences('cleaned_peptides.fasta')
+    main()
